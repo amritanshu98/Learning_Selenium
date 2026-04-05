6c1a717968a19649319840deed2b41e8e9e7d1a8
amritanshu98 <amrit0498@gmail.com>
2026-04-05 14:15:07 +0530

    Selenium_Practice

--- Practice_Codes/test_sele_Lab021.py
+++ Practice_Codes/test_sele_Lab021.py
@@ -2,20 +2,20 @@ import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.alert import Alert
+
 
 class TestCheckboxes(object):
     @pytest.mark.qa
-    def test_alerts_tc1_checkbox(self):
+    def test_tc1_checkbox(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.page_load_strategy = "normal"
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.maximize_window()
         self.driver.get("https://the-internet.herokuapp.com/checkboxes")
+        time.sleep(3)
         checkboxes = self.driver.find_elements(By.XPATH, "//input[@type='checkbox']")
         checkboxes[0].click()
         time.sleep(5)
+        print("Clicked !!!")
 
         self.driver.quit()
